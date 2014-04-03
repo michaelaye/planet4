fcab6ef489baed0e33940828caa6948ded9f73d5
K.-Michael Aye <aye@ess.ucla.edu>
2014-04-03 04:42:48 -0700

    finishing argparsing for data reduction. Help doc is complete.

--- planet4_reduction.py
+++ planet4_reduction.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 from __future__ import print_function, division
 import pandas as pd
 import sys
@@ -18,17 +19,20 @@ def is_okay(row):
         return True
 
 
-def main(fname, parse_times=True, do_filter=True):
+def main(fname, raw_times=False, keep_dirt=False):
     fname_base = os.path.basename(fname)
     root = os.path.dirname(fname)
     fname_no_ext = os.path.splitex(fname_base)[0]
     reader = pd.read_csv(fname, chunksize=1e6, na_values=['null'])
     data = [chunk for chunk in reader]
     df = pd.concat(data, ignore_index=True)
-    df.acquisition_date = pd.to_datetime(df.acquisition_date)
-    df['okay'] = True # prefill
-    df['okay'] = df.apply(is_okay, axis=1)
-    df = df[df.okay]
+    if not raw_times:
+        df.acquisition_date = pd.to_datetime(df.acquisition_date)
+    if not keep_dirt:
+        df['okay'] = True # prefill
+        df['okay'] = df.apply(is_okay, axis=1)
+        df = df[df.okay]
+        df.drop('okay', axis=1)
     df.to_hdf(os.path.join(root, fname_no_ext+'.h5'),
               'df')
 
@@ -37,26 +41,14 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('csv_fname', help="Provide the filename of the database "
                                            "dump csv-file here.")
+    parser.add_argument('--raw_times', help="Do not parse the times into a Python datetime"
+                                            " object. For the stone-age. ;) Default:"
+                                            " parse into datetime object.",
+                        action='store_true')
+    parser.add_argument('--keep_dirt', help="Do not filter for dirty data. Keep everything."
+                                            " Default: Do the filtering.",
+                        action='store_true')
     args = parser.parse_args()
-    print(args.csv_fname)
-    # try:
-    #     fname = sys.argv[1]
-    # except IndexError:
-    #     print("Run like so: {} csv-file".format)
-    #     sys.exit()
-    # parse_times = do_filter = True
-    # try:
-    #     second_option = sys.argv[2]
-    #     if second_option == 'dont_parse_times':
-    #         parse_times = False
-    #     elif second_option == 'dont_filter':
-    #         do_filter = False
-    #     else:
-    #         print("Second option should be either 'dont_filter' or 'dont_parse_times'.")
-    #         sys.exit()
-    # except:
-    #     pass
-    # try:
-    #     third_option = sys.argv[3]
-    #     if third_option == 'dont_parse_times'    
-    # main()
+    print(args)
+    main(args.csv_fname, args.raw_times, args.keep_dirt)
+    
