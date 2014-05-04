37a7743429bdfa25a59c161da76ec68cadb95177
K.-Michael Aye <kmichael.aye@gmail.com>
2014-05-04 17:31:00 -0600

    pep8ified

--- overplot_blotches.py
+++ overplot_blotches.py
@@ -1,5 +1,4 @@
 from __future__ import print_function, division
-from P4_sandbox import helper_functions as hf
 from P4_sandbox.get_data import get_image_from_record
 import os
 import pandas as pd
@@ -10,9 +9,10 @@ import sys
 
 data_root = '/Users/maye/data/planet4'
 
+
 def data_munging(img_id):
     print("Reading current marked data.")
-    data = pd.read_hdf(os.path.join(data_root, 'marked.h5'),'df',
+    data = pd.read_hdf(os.path.join(data_root, 'marked.h5'), 'df',
                        where='image_id=='+img_id)
     print("Done.")
     return data
@@ -25,10 +25,18 @@ def get_blotches(data, img_id):
     return blotches
 
 
+def get_fans(data, img_id):
+    """get the fans for given image_id"""
+    subframe = data[data.image_id == img_id]
+    fans = subframe[subframe.marking == 'fan']
+    return fans
+
+
 def get_image_name_from_data(data):
     """This assumes that all data is for the same image!"""
     line = data.iloc[0]
     return os.path.basename(line.image_url.strip())
+
 
 def add_ellipses_to_axis(ax, blotches):
     colors = cycle('bgrcmyk')
@@ -36,10 +44,11 @@ def add_ellipses_to_axis(ax, blotches):
         line = blotches.iloc[i]
         plt.scatter(line.x, line.y, color=color)
         el = Ellipse((line.x, line.y),
-                 line.radius_1, line.radius_2, line.angle,
-                 fill=False, color=color, linewidth=1)
+                     line.radius_1, line.radius_2, line.angle,
+                     fill=False, color=color, linewidth=1)
         ax.add_artist(el)
-    
+
+
 def plot_blotches(data, img_id):
     blotches = get_blotches(data, img_id)
     # this will endlessly cycle through the colors given
@@ -59,4 +68,4 @@ if __name__ == '__main__':
     # img id should be given on command line
     img_id = sys.argv[1]
     data = data_munging(img_id)
-    main(data, img_id)
+    plot_blotches(data, img_id)
