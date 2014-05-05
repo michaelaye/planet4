2377cdd2287697e0644700bbe0e6c81355558ded
K.-Michael Aye <kmichael.aye@gmail.com>
2014-05-04 20:52:58 -0700

    pep8ified

--- get_data.py
+++ get_data.py
@@ -8,15 +8,16 @@ import matplotlib.image as mplimg
 data_root = '/Users/maye/data/planet4'
 done_path = os.path.join(data_root, 'done.h5')
 
+
 def get_example_blotches():
-    return pd.read_hdf(os.path.join(data_root,'blotch_data.h5'),'df')
+    return pd.read_hdf(os.path.join(data_root, 'blotch_data.h5'), 'df')
 
 
 def get_image_from_record(line):
     """Download image if not there yet and return numpy array.
-    
+
     Takes a data record (called 'line'), picks out the image_url.
-    First checks if the name of that image is already stored in 
+    First checks if the name of that image is already stored in
     the image path. If not, it grabs it from the server.
     Then uses matplotlib.image to read the image into a numpy-array
     and finally returns it.
@@ -37,26 +38,30 @@ def get_image_from_record(line):
 
 def get_current_database(with_tutorial=False):
     if with_tutorial:
-        df = pd.read_hdf('/Users/maye/data/planet4/2014-02-02_planet_four_classifications.h5',
-                     'df')
+        df = pd.read_hdf('/Users/maye/data/planet4/'
+                         '2014-02-02_planet_four_classifications.h5',
+                         'df')
     else:
-        df = pd.read_hdf('/Users/maye/data/planet4/2014-02-02_tutorial_id_removed.h5',
-                            'df')
+        df = pd.read_hdf('/Users/maye/data/planet4/'
+                         '2014-02-02_tutorial_id_removed.h5',
+                         'df')
     define_season_column(df)
     return df
 
 
 def get_current_marked():
-    return pd.read_hdf(os.path.join(data_root, 'marked.h5'),'df')
+    return pd.read_hdf(os.path.join(data_root, 'marked.h5'), 'df')
 
 
 def get_current_done():
     return pd.read_hdf(done_path, 'df')
+
+
 def get_and_save_done(df, limit=30):
     counts = classification_counts_per_image(df)
     ids_done = counts[counts >= limit].index
     df[df.image_id.isin(ids_done)].to_hdf(done_path, 'df')
-    
+
 
 ###
 ### Season related stuff
