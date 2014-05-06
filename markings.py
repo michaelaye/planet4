c22fe92760a24cd39b6782b01ed4e05a2fb88cfc
K.-Michael Aye <kmichael.aye@gmail.com>
2014-05-06 16:28:16 -0700

    adding P4_Fan derived from Line2D.

--- markings.py
+++ markings.py
@@ -1,9 +1,18 @@
 from matplotlib.patches import Ellipse
-from itertools import cycle
 import pandas as pd
 import os
+from math import sin, cos, radians
+import sys
+import urllib
+import shutil
 import matplotlib.pyplot as plt
+import matplotlib.image as mplimg
+import matplotlib.lines as lines
+
 data_root = '/Users/maye/data/planet4'
+
+img_x_size = 840
+img_y_size = 648
 
 
 class P4_ImgID(object):
@@ -14,6 +23,28 @@ class P4_ImgID(object):
         self.data = pd.read_hdf(os.path.join(data_root, 'marked.h5'), 'df',
                                 where='image_id=='+imgid)
 
+    def get_subframe(self):
+        """Download image if not there yet and return numpy array.
+
+        Takes a data record (called 'line'), picks out the image_url.
+        First checks if the name of that image is already stored in
+        the image path. If not, it grabs it from the server.
+        Then uses matplotlib.image to read the image into a numpy-array
+        and finally returns it.
+        """
+        url = self.data.iloc[0].image_url
+        targetpath = os.path.join(data_root, 'images', os.path.basename(url))
+        if not os.path.exists(targetpath):
+            print("Did not find image in cache. Downloading ...")
+            sys.stdout.flush()
+            path = urllib.urlretrieve(url)[0]
+            print("Done.")
+            shutil.move(path, targetpath)
+        else:
+            print("Found image in cache.")
+        im = mplimg.imread(targetpath)
+        return im
+
     def get_fans(self):
         return self.data[self.data.marking == 'fan']
 
@@ -21,21 +52,24 @@ class P4_ImgID(object):
         return self.data[self.data.marking == 'blotch']
 
 
-class P4_Blotch(object):
-    """docstring for P4_Blotch"""
+class P4_Marking(object):
+    """Base class for P4 markings."""
+    def __init__(self, json_row):
+        super(P4_Marking, self).__init__()
+        self.data = json_row
 
-    def __init__(self, json_line):
-        super(P4_Blotch, self).__init__()
-        self.line = json_line
 
-    def scatter(self, ax, color='b'):
-        ax.scatter(self.line.x, self.line.y, color=color,
+class P4_Blotch(P4_Marking):
+    """Blotch management class for P4."""
+
+    def plot_center(self, ax, color='b'):
+        ax.scatter(self.data.x, self.data.y, color=color,
                    s=20, c='b', marker='o')
 
     def get_ellipse(self, color='b'):
-        return Ellipse((self.line.x, self.line.y),
-                       self.line.radius_1, self.line.radius_2,
-                       self.line.angle,
+        return Ellipse((self.data.x, self.data.y),
+                       self.data.radius_1, self.data.radius_2,
+                       self.data.angle,
                        fill=False, linewidth=1, color=color)
 
     def add_ellipse(self, ax, color='b', ellipse=None):
@@ -43,3 +77,35 @@ class P4_Blotch(object):
             ellipse = self.get_ellipse()
         ellipse.set_color(color)
         ax.add_artist(ellipse)
+        # do this better later in a plot controller
+        ax.set_xlim(0, img_x_size)
+        ax.set_ylim(0, img_y_size)
+
+
+class P4_Fan(lines.Line2D):
+    """Fan management class for P4. """
+
+    def __init__(self, json_row):
+        self.data = json_row
+        # first coordinate is the base of fan
+        self.x = self.data.x
+        self.y = self.data.y
+        inside_half = self.data.spread / 2.0
+        alpha = self.data.angle - inside_half
+        length = self.data.distance / cos(radians(inside_half))
+        beta = alpha + self.data.spread
+        # first arm
+        self.line_x = [self.x + length * cos(radians(alpha))] + [self.x]
+        self.line_y = [self.y + length * sin(radians(alpha))] + [self.y]
+        # second arm
+        self.line_x += [self.x + length * cos(radians(beta))]
+        self.line_y += [self.y + length * sin(radians(beta))]
+        lines.Line2D.__init__(self, self.line_x, self.line_y)
+        ax = plt.gca()
+        ax.set_xlim(0, img_x_size)
+        ax.set_ylim(0, img_y_size)
+
+    def __str__(self):
+        out = 'x: {0}\ny: {1}\nline_x: {2}\nline_y: {3}'\
+            .format(self.x, self.y, self.line_x, self.line_y)
+        return out
