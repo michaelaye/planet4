e2c629fe82ddbe907e9823ae22070a8c3aefb87e
K.-Michael Aye <kmichael.aye@gmail.com>
2015-08-04 18:28:45 -0600

    Changing perform_dbscan to a DBScanner class for better readability. Adding error to exceptions in case of empty class.

--- planet4/clustering.py
+++ planet4/clustering.py
@@ -5,111 +5,107 @@ from sklearn.cluster import DBSCAN
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
-from .exceptions import *
+from .exceptions import NoDataToClusterError
 
 matplotlib.style.use('bmh')
 
-ellipse_cols = 'x y radius_1 radius_2 angle'.split()
-fan_cols = 'x y angle spread distance'.split()
 
+class DBScanner(object):
 
-def get_mean_marking(data, label_members, kind='fans'):
-    if kind == 'blotches':
-        clusterdata = data[ellipse_cols].iloc[label_members]
-    elif kind == 'fans':
-        clusterdata = data[fan_cols].iloc[label_members]
-    else:
-        raise UnknownMarkingKindError(kind)
-    meandata = clusterdata.mean()
-    if kind == 'blotches':
-        val = markings.Blotch(meandata)
-    elif kind == 'fans':
-        val = markings.Fan(meandata)
-    return val
+    marking_cols = {'fan': 'angle spread distance'.split(),
+                    'blotch': 'angle radius_1 radius_2'.split()}
+    MarkingClass = {'fan': markings.Fan,
+                    'blotch': markings.Blotch}
 
+    def __init__(self, data, kind, eps=10, min_samples=3,
+                 coords=['x', 'y'],
+                 ax=None, linestyle='-', quiet=True):
+        self.data = data
+        self.kind = kind  # fans or blotches
+        self.eps = eps
+        self.min_samples = min_samples
+        self.coords = coords
+        self.ax = ax
+        self.linestyle = linestyle
+        self.quiet = quiet
 
-def perform_dbscan(p4img, kind='fans', current_axis=None, eps=10, min_samples=3,
-                   linestyle='-', quiet=True):
-    """Cluster data for one P4 image id.
+        self.get_current_X()
+        self.run_DBSCAN()
+        self.post_analysis()
 
-    Parameters
-    ----------
-    p4img : planet4.ImageID
-        ImageID object for this clustering run.
-    kind : string
-        control flow parameter for kind of object to cluster
-    current_axis : matplotlib axis
-        when provided, plotting is done
-    eps : int
-        DBSCAN parameter, max distance in clustering space units for inclusion
-        into cluster
-    min_samples : int
-        Minimum number of cluster members to make up a cluster
-    linestyle : char
-        Matplotlib line style control character
-    quiet : boolean
-        Controlling if feedback is being printed to stdout.
+    def get_current_X(self):
+        # defaults for DBSCAN
+        current_X = self.data[self.coords].values
+        if len(current_X) == 0:
+            raise NoDataToClusterError
+        self.current_X = current_X
 
-    """
-    if kind=='fans':
-        current_data = p4img.get_fans()
-    elif kind=='blotches':
-        current_data = p4img.get_blotches()
-    else:
-        raise UnknownMarkingKindError(kind)
+    def run_DBSCAN(self):
+        db = DBSCAN(self.eps, self.min_samples).fit(self.current_X)
+        labels = db.labels_.astype('int')
+        self.core_samples = db.core_sample_indices_
+        unique_labels = set(labels)
+        self.n_clusters = len(unique_labels) - (1 if -1 in labels else 0)
+        self.labels = labels
+        self.unique_labels = unique_labels
+        if not self.quiet:
+            print("Estimated number of clusters:", self.n_clusters)
 
-    center_only = ['x', 'y']
-    # center_and_radii = 'x y radius_1 radius_2'.split()
-    current_X = current_data[center_only].values
-
-    # there could be no markings! Returning None then.
-    if len(current_X) == 0:
-        return None
-    db = DBSCAN(eps=eps, min_samples=min_samples).fit(current_X)
-    labels = db.labels_.astype('int')
-    core_samples = db.core_sample_indices_
-    # number of clusters in labels, ignoring noise if present
-    n_clusters = len(set(labels)) - (1 if -1 in labels else 0)
-    if not quiet:
-        print("Estimated number of clusters:", n_clusters)
-
-    unique_labels = set(labels)
-    colors = plt.cm.Spectral(np.linspace(0, 1, len(unique_labels)))
-    reduced_data = []
-    for k, color in zip(unique_labels, colors):
-        label_members = [index[0] for index in np.argwhere(labels == k)]
-
-        # only do the following if we have a matplotlib axis
-        if current_axis:
+    def post_analysis(self):
+        colors = plt.cm.Spectral(np.linspace(0, 1, len(self.unique_labels)))
+        reduced_data = []
+        n_rejected = 0
+        for k, color in zip(self.unique_labels, colors):
+            label_members = [i[0] for i in np.argwhere(self.labels == k)]
             if k == -1:
-                color = 'w'
+                n_rejected = len(label_members)
+            if self.ax:
+                self.process_plotting(k, label_members)
+            if k > -0.5:  # == if not noise marking
+                cluster = self.get_mean_marking(label_members)
+                cluster.n_members = len(label_members)
+                reduced_data.append(cluster)
+                if self.ax:
+                    self.process_cluster_plotting(cluster, color)
+            if self.ax:
+                markings.set_subframe_size(self.ax)
+        self.reduced_data = reduced_data
+        self.n_rejected = n_rejected
+
+    @property
+    def n_reduced_data(self):
+        return len(self.reduced_data)
+
+    def get_mean_marking(self, label_members):
+        # what columns to pick for averaging. Depends on
+        # what kind (fans/blotches) we have:
+        cols = self.coords + self.marking_cols[self.kind]
+        clusterdata = self.data[cols].iloc[label_members]
+        meandata = clusterdata.mean()
+        return self.MarkingClass[self.kind](meandata)
+
+    def process_cluster_plotting(self, cluster, color):
+        cluster.set_color(color)
+        if self.kind == 'blotches':
+            self.ax.add_artist(cluster)
+        else:
+            self.ax.add_line(cluster)
+            cluster.add_semicircle(self.ax, color=color)
+            cluster.add_mean_wind_pointer(self.ax, color=color,
+                                          ls=self.linestyle)
+
+    def process_plotting(self, k, label_members):
+        if k == -1:  # process noise markers
+            color = 'w'
+            markersize = 5
+        for i in label_members:
+            x = self.current_X[i]
+            if i in self.core_samples and k != -1:
+                markersize = 8
+            else:
                 markersize = 5
-            for index in label_members:
-                x = current_X[index]
-                if index in core_samples and k != -1:
-                    markersize = 8
-                else:
-                    markersize = 5
-                current_axis.plot(x[0], x[1], 'o', markerfacecolor=color,
-                                  markeredgecolor='k', markersize=markersize)
-        # do this part always to get the mean ellipse data
-        if k > -0.5:
-            el = get_mean_marking(current_data, label_members, kind=kind)
-            el.n_members = len(label_members)
-            reduced_data.append(el)
-            if current_axis:
-                el.set_color(color)
-                if kind == 'blotches':
-                    current_axis.add_artist(el)
-                else:
-                    current_axis.add_line(el)
-                    el.add_semicircle(current_axis, color=color)
-                    el.add_mean_wind_pointer(current_axis,
-                                             color=color,
-                                             ls=linestyle)
-        if current_axis:
-            markings.set_subframe_size(current_axis)
-    return reduced_data
+            self.ax.plot(x[0], x[1], 'o', markerfacecolor=color,
+                         markedgecolor='k', markersize=markersize)
 
 
 def gold_star_plotter(gold_id, axis, blotches=True, kind='blotches'):
@@ -147,17 +143,15 @@ def main():
     # citizen stuff
     p4img.plot_fans(ax=axes[0])
     axes[0].set_title('Citizen Markings')
-    perform_dbscan(p4img.get_fans(), axes[1],
-                   eps=7,
-                   min_samples=5, fans=True, ls='-')
+    DBScanner(p4img.get_fans(), 'fan', ax=axes[1], eps=7, min_samples=5,
+              linestyle='-')
     axes[1].set_title('All citizens clusters (including science team)')
 
     # gold stuff
     gold_star_plotter(p4img, axes[2], fans=True, blotches=False)
     axes[2].set_title('Science team markings')
-    perform_dbscan(golddata, axes[1],
-                   min_samples=2,
-                   eps=11, fans=True, ls='--')
+    DBScanner(golddata, 'fan', ax=axes[1], min_samples=2, eps=11,
+              linestyle='--')
     axes[3].set_title('Science team clusters')
 
     plt.show()
--- planet4/exceptions.py
+++ planet4/exceptions.py
@@ -2,16 +2,27 @@ class PlanetFourError(Exception):
     """Base class for exceptions in this module."""
     pass
 
+
 class NoFilesFoundError(PlanetFourError):
 
     def __str__(self):
         return "Search or glob for files returned empty."
 
 
+class NoDataToClusterError(PlanetFourError):
+
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __str__(self):
+        return "No data to scan was found in obj: {}".format(self.obj)
+
+
 class UnknownMarkingKindError(PlanetFourError):
 
     def __init__(self, kind):
         self.kind = kind
+
     def __str__(self):
         return """An unknown marking kind was set: {}. Usually this should be
  one of 'fans' or 'blotches'.""".format(self.kind)
