1962ae0ffdc8075aa044497e382bfff2f55a4e9f
K.-Michael Aye <kmichael.aye@gmail.com>
2015-12-17 14:52:35 -0700

    moved former ResultManager as PathManager to io module

--- planet4/plotting.py
+++ planet4/plotting.py
@@ -1,49 +1,19 @@
-import pandas as pd
-from pathlib import Path
+import matplotlib.pyplot as plt
 
-from . import markings
+from .io import PathManager
+from .markings import ImageID
 
 
-class ResultManager:
+def plot_image_id_pipeline(image_id, data):
+    imgid = ImageID(image_id, data)
 
-    def __init__(self, id_, datapath, suffix='.hdf'):
-        self.id_ = id_
-        self.inpath = Path(datapath)
-        self.suffix = suffix
-        self.unfnotched_path = self.inpath.with_name(self.inpath.stem+'_unfnotched')
+    fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 8))
+    axes = axes.ravel()
+    for ax in axes:
+        imgid.show_subframe(ax=ax)
+    imgid.plot_fans(ax=axes[1])
+    imgid.plot_blotches(ax=axes[2])
 
-    def fanfile(self, unfnotched=False):
-        if unfnotched:
-            path = self.unfnotched_path
-        else:
-            path = self.inpath
-        return path / (self.id_ + '_fans' + self.suffix)
-
-    def fandf(self, unfnotched=False):
-        return pd.read_hdf(str(self.fanfile(unfnotched)))
-
-    def fans(self, unfnotched=False):
-        fandf = self.fandf(unfnotched)
-        return [markings.Fan(i) for _, i in fandf.iterrows()]
-
-    def blotchfile(self, unfnotched=False):
-        if unfnotched:
-            path = self.unfnotched_path
-        else:
-            path = self.inpath
-        return path / (self.id_ + '_blotches' + self.suffix)
-
-    def blotchdf(self, unfnotched=False):
-        return pd.read_hdf(str(self.blotchfile(unfnotched)))
-
-    def blotches(self, unfnotched=False):
-        blotchdf = self.blotchdf(unfnotched)
-        return [markings.Blotch(i) for _, i in blotchdf.iterrows()]
-
-    @property
-    def fnotchfile(self):
-        return self.inpath / (self.id_ + '_fnotches' + self.suffix)
-
-    @property
-    def fnotchdf(self):
-        return pd.read_hdf(str(self.fnotchfile))
+    for ax in axes:
+        ax.set_axis_off()
+    plt.show()
