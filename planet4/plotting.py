23703f2f76a27b67d87a047e858b8d3f4ad3c662
K.-Michael Aye <kmichael.aye@gmail.com>
2016-01-29 16:16:41 -0700

    Removing clustering from plotting of pipeline
    The point is, I don’t want the plotter to be able to execute the
    pipeline,
    so even so it’s more code in the notebook, I rather have it separated
    for
    better control.
    I now also check if things exist before I plot, so that it doesn’t
    choke in that case.

--- planet4/plotting.py
+++ planet4/plotting.py
@@ -6,17 +6,9 @@ from . import clustering
 
 
 def plot_image_id_pipeline(image_id, **kwargs):
-    cm = clustering.ClusteringManager(id_=image_id, **kwargs)
-    cm.cluster_image_id(image_id)
-    pm = cm.pm
+    pm = io.PathManager(id_=image_id, **kwargs)
 
     imgid = markings.ImageID(image_id)
-
-    reduced_fans = markings.FanContainer.from_fname(pm.reduced_fanfile)
-    reduced_blotches = markings.BlotchContainer.from_fname(pm.reduced_blotchfile)
-
-    finalfans = markings.FanContainer.from_fname(pm.final_fanfile)
-    finalblotches = markings.BlotchContainer.from_fname(pm.final_blotchfile)
 
     fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(10, 8))
     axes = axes.ravel()
@@ -24,10 +16,19 @@ def plot_image_id_pipeline(image_id, **kwargs):
         imgid.show_subframe(ax=ax)
     imgid.plot_fans(ax=axes[1])
     imgid.plot_blotches(ax=axes[2])
-    imgid.plot_fans(ax=axes[4], fans=reduced_fans.content)
-    imgid.plot_blotches(ax=axes[5], blotches=reduced_blotches.content)
-    imgid.plot_fans(ax=axes[3], fans=finalfans.content)
-    imgid.plot_blotches(ax=axes[3], blotches=finalblotches.content)
+
+    if pm.reduced_fanfile.exists():
+        reduced_fans = markings.FanContainer.from_fname(pm.reduced_fanfile)
+        imgid.plot_fans(ax=axes[4], fans=reduced_fans.content)
+    if pm.reduced_blotchfile.exists():
+        reduced_blotches = markings.BlotchContainer.from_fname(pm.reduced_blotchfile)
+        imgid.plot_blotches(ax=axes[5], blotches=reduced_blotches.content)
+    if pm.final_fanfile.exists():
+        finalfans = markings.FanContainer.from_fname(pm.final_fanfile)
+        imgid.plot_fans(ax=axes[3], fans=finalfans.content)
+    if pm.final_blotchfile.exists():
+        finalblotches = markings.BlotchContainer.from_fname(pm.final_blotchfile)
+        imgid.plot_blotches(ax=axes[3], blotches=finalblotches.content)
 
     for ax in axes:
         ax.set_axis_off()
@@ -35,10 +36,6 @@ def plot_image_id_pipeline(image_id, **kwargs):
     fig.subplots_adjust(left=None, top=None, bottom=None, right=None,
                         wspace=0.001, hspace=0.001)
     plt.show()
-    if pm:
-        return pm
-    else:
-        return cm
 
 
 def plot_raw_fans(id_):
