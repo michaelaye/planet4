513926d3def2a92b9a62d03d0077dae12877ce55
K.-Michael Aye <kmichael.aye@gmail.com>
2016-02-22 14:51:41 -0700

    implement pypet parameter search and analysis
    Reduced parameter search to vmax, incline and vent_radius.

--- notebooks/vpython/2016-02-14 VPython ballistic.py
+++ notebooks/vpython/2016-02-14 VPython ballistic.py
@@ -39,6 +39,7 @@ class FanSimulator(object):
     up = vec(0, 0, 1)
     
     def __init__(self, N, vent_radius=0.5, vmax=50, dt=1e-2, location='mars'):
+        np.random.seed(42)
         self.N = N
         self.dt = dt
         self.vent_radius = vent_radius
@@ -101,20 +102,22 @@ class FanSimulator(object):
                     if p.pos.z < start.z:
                         p.update = False
 
-    def plot(self, save=False):
+    def plot(self, save=False, equal=True):
         fig, axes = plt.subplots(ncols=1, squeeze=False)
         axes = axes.ravel()
         axes[0].scatter(self.positions[:,0], self.positions[:,1], 5)
         for ax in axes:
-            ax.set_aspect('equal')
+            if equal:
+                ax.set_aspect('equal')
             ax.set_xlabel('Distance [m]')
             ax.set_ylabel('Spread [m]')
-        ax.set_title("{0} particles, v0_z={1}, v0_x= {2}, jitter={3} [m/s]"
-                     .format(self.N, self.vmax, self.incline, self.jitter))
+        ax.set_title("{0} particles, v0_z={1}, v0_x= {2}, jitter={3} [m/s]\n"
+                     "dt={4}"
+                     .format(self.N, self.vmax, self.incline, self.jitter, self.dt))
         if save:
             root = "/Users/klay6683/Dropbox/SSW_2015_cryo_venting/figures/"
-            fig.savefig(root+'fan_{}_{}_{}_{}.png'
-                             .format(self.N, self.vmax, self.incline, self.jitter),
+            fig.savefig(root+'fan_vmax{}_incline{}_vent_radius{}.png'
+                             .format(self.vmax, self.incline, self.vent_radius),
                         dpi=150) 
     
     def init_vpython(self):
@@ -137,7 +140,7 @@ class FanSimulator(object):
 
 # In[ ]:
 
-get_ipython().magic('matplotlib inline')
+#%matplotlib nbagg
 
 import seaborn as sns
 sns.set_context('notebook')
@@ -145,14 +148,26 @@ sns.set_context('notebook')
 
 # In[ ]:
 
-sim = FanSimulator(5000, vent_radius=0.5)
+sim = FanSimulator(5000, vent_radius=0.1, dt=0.01)
 sim.init_positions()
 sim.init_velocities()
-sim.incline_and_vary_jet()
+sim.incline_and_vary_jet(jitter=0.2, incline=10.0)
 
 sim.loop()
 
-sim.plot(save=True)
+sim.plot(save=True, equal=False)
+
+
+# In[ ]:
+
+sim = FanSimulator(5000, vent_radius=0.1, dt=0.001)
+sim.init_positions()
+sim.init_velocities()
+sim.incline_and_vary_jet(jitter=0.2, incline=10.0)
+
+sim.loop()
+
+sim.plot(save=True, equal=False)
 
 
 # In[ ]:
@@ -162,6 +177,15 @@ from pypet import Environment, cartesian_product
 
 # In[ ]:
 
+def add_parameters(traj, dt=1e-2):
+    traj.f_add_parameter('N', 5000, comment='number of particles')
+    traj.f_add_parameter('vent_radius', 0.5, comment='radius of particle emitting vent')
+    traj.f_add_parameter('vmax', 50, comment='vmax in center of vent')
+    traj.f_add_parameter('dt', dt, comment='dt of simulation')
+    traj.f_add_parameter('incline', 10.0, comment='inclining vx value')
+    traj.f_add_parameter('jitter', 0.1, comment='random x,y jitter for velocities')
+    traj.f_add_parameter('location', 'mars', comment='location determining g-force')
+
 def run_simulation(traj):
     sim = FanSimulator(traj.N, vent_radius=traj.vent_radius, vmax=traj.vmax,
                        dt=traj.dt, location=traj.location)
@@ -169,31 +193,25 @@ def run_simulation(traj):
     sim.init_velocities()
     sim.incline_and_vary_jet(incline=traj.incline, jitter=traj.jitter)
     sim.loop()
+    sim.plot(save=True, equal=False)
     traj.f_add_result('positions', sim.positions, comment='End positions of particles')
     traj.f_add_result('t', sim.t, comment='duration of flight')
 
 env = Environment(trajectory='FanSimulation', filename='./pypet/',
                   large_overview_tables=True,
-                 add_time=True,
-                 multiproc=True,
-                 ncores=3,
-                 log_config='DEFAULT')
+                  add_time=True,
+                  multiproc=False,
+                  ncores=6,
+                  log_config='DEFAULT')
 
 traj = env.v_trajectory
-traj.f_add_parameter('N', 5000, comment='number of particles')
-traj.f_add_parameter('vent_radius', 0.5, comment='radius of particle emitting vent')
-traj.f_add_parameter('vmax', 50, comment='vmax in center of vent')
-traj.f_add_parameter('dt', 1e-2, comment='dt of simulation')
-traj.f_add_parameter('incline', 1.0, comment='inclining vx value')
-traj.f_add_parameter('jitter', 0.1, comment='random x,y jitter for velocities')
-traj.f_add_parameter('location', 'mars', comment='location determining g-force')
 
-explore_dict = {'vent_radius':np.arange(0.1, 1, 0.1).tolist(),
-                'vmax':range(10, 100, 10),
-                'dt':[1e-2, 1e-3],
-                'incline':[0.1, 1.0, 5.0, 10.0],
-                'jitter':[0.05, 0.1, 0.2, 0.3],
-                'location':['mars','earth']}
+add_parameters(traj, dt=1e-2)
+
+explore_dict = {'vent_radius':[0.1, 0.5, 1.0],
+                'vmax':[10, 50, 100],
+                'incline':[0.1, 1.0, 5.0]}
+
 to_explore = cartesian_product(explore_dict)
 traj.f_explore(to_explore)
 
---
+++ notebooks/vpython/2016-02-19 pypet analysis.py
@@ -0,0 +1,123 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+from pypet import Trajectory
+
+
+# In[ ]:
+
+traj = Trajectory(filename='./pypet/FanSimulation_2016_02_22_14h46m09s.hdf5')
+
+
+# In[ ]:
+
+traj.f_load(index=-1, load_parameters=2, load_results=2)
+
+
+# In[ ]:
+
+traj.f_get_parameters()
+
+
+# In[ ]:
+
+traj.f_get_explored_parameters()
+
+
+# In[ ]:
+
+traj.f_get_run_names()
+
+
+# In[ ]:
+
+def my_filter_function(location,dt):
+    result = location =='mars' and dt=1e-2
+    return result
+
+
+# In[ ]:
+
+set(traj.f_get('incline').f_get_range())
+
+
+# In[ ]:
+
+def filter_function(loc,dt,vmax,vent_radius,incline):
+    result = loc=='mars' and dt==1e-2 and vmax==50        and vent_radius==0.5 and incline==1.0
+    return result
+
+
+# In[ ]:
+
+def standard_filter(loc, dt, jitter, incline):
+    result = loc=='mars' and dt==1e-2 and jitter==0.1 and incline!=10.0
+    return result
+
+
+# In[ ]:
+
+idx_iter = traj.f_find_idx(['parameters.location','parameters.dt',
+                            'parameters.jitter', 'parameters.incline'], standard_filter)
+
+
+# In[ ]:
+
+indexes = list(idx_iter)
+
+
+# In[ ]:
+
+indexes
+
+
+# In[ ]:
+
+get_ipython().magic('matplotlib nbagg')
+
+
+# In[ ]:
+
+def plot_data(i):
+
+    traj.v_idx = indexes[i]
+    data = traj.res.crun.positions
+    title = ''
+    for k,v in traj.f_get_parameters(fast_access=True).items():
+        key = k.split('.')[1]
+        if key=='location' or key=='dt' or key=='N' or key=='jitter':
+            continue
+        t = "{}:{} | ".format(key, v)
+        title += t
+    fig, axes = plt.subplots(nrows=2, squeeze=False)
+    axes = axes.ravel()
+    for ax in axes:
+        ax.scatter(data[:, 0], data[:, 1])
+    axes[1].set_xlim(-15, 70)
+    axes[1].set_ylim(-20, 20)
+    fig.suptitle(title[:-2], fontsize=13)
+
+
+# In[ ]:
+
+get_ipython().magic('matplotlib inline')
+import seaborn as sns
+sns.set_context('notebook')
+
+
+# In[ ]:
+
+from ipywidgets import interact
+
+
+# In[ ]:
+
+interact(plot_data, i=(0, len(indexes)-1));
+
+
+# In[ ]:
+
+
+
