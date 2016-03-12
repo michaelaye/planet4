0b13a709e74eb3dd369d8491a749c33aeafdaec2
K.-Michael Aye <kmichael.aye@gmail.com>
2016-03-12 15:29:21 -0700

    wip

--- notebooks/test_pipeline.py
+++ notebooks/test_pipeline.py
@@ -1,12 +1,16 @@
 
 # coding: utf-8
 
-# In[ ]:
+# # are you in the right git branch?
+# As long as the dynamic min_samples feature is not merged into `master`, make sure your code
+# is at the right branch before trying to test dynamic samples.
+
+# In[8]:
 
 cd ~/Dropbox/CTX_to_jpg/pipeline_check/
 
 
-# In[ ]:
+# In[9]:
 
 from glob import glob
 
@@ -16,24 +20,54 @@ ids = [i.split('_')[0] for i in fnames]
 ids[:5]
 
 
-# In[ ]:
+# In[10]:
 
 from planet4 import clustering, io, markings, helper_functions as hf
 from pathlib import Path
 
 
-# In[ ]:
+# In[26]:
 
-path = Path("/Users/klay6683/data/planet4/pipelinecheck3")
+path = Path("/Users/klay6683/data/planet4/ice_free_check")
 path.mkdir(exist_ok=True)
 cm = clustering.ClusteringManager(fnotched_dir=path,
                                  include_angle=True, include_distance=False, 
                                  include_radius=False, eps=10, min_distance=20)
 
 
-# In[ ]:
+# In[27]:
+
+cm.cluster_image_name('ESP_022699_0985')
+
+
+# In[12]:
 
 from planet4 import plotting
+
+
+# In[20]:
+
+db = io.DBManager()
+
+
+# In[21]:
+
+data = db.get_image_name_markings('ESP_022699_0985')
+
+
+# In[22]:
+
+data.info()
+
+
+# In[25]:
+
+data[data.marking=='blotch'].info()
+
+
+# In[ ]:
+
+
 
 
 # In[ ]:
@@ -59,16 +93,18 @@ def process_imgid(id_):
     return id_
 
 
-# In[ ]:
+# In[14]:
 
-get_ipython().magic('matplotlib nbagg')
+get_ipython().magic(u'matplotlib nbagg')
 
 
-# In[ ]:
+# In[19]:
 
-imid = '1c5'
-cm.cluster_image_id(imid)
-plotting.plot_image_id_pipeline(imid, datapath=path, save=True)
+recheck_ids = ['1dn','1k3','1e4','1fe','1aa','225','1pr','19g']
+for imid in recheck_ids:
+    print(imid)
+    cm.cluster_image_id(imid)
+    plotting.plot_image_id_pipeline(imid, datapath=path, save=True)
 
 
 # In[ ]:
