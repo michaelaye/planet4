f91418b790293d4e896755ea4ba899a4a6dd0140
K.-Michael Aye <kmichael.aye@gmail.com>
2016-07-07 15:37:29 +0800

    add some docstrings and improve functionality

--- planet4/plotting.py
+++ planet4/plotting.py
@@ -23,11 +23,14 @@ def plot_image_id_pipeline(image_id, dbname=None, datapath=None, save=False,
     for ax in axes:
         ax.set_axis_off()
 
+    fig.suptitle(image_id + ', ' + str(figtitle))
     fig.subplots_adjust(left=None, top=None, bottom=None, right=None,
                         wspace=0.001, hspace=0.001)
     if save:
         fname = "{}_{}.pdf".format(imgid.imgid, savetitle)
-        fpath = pm.output_dir / fname
+        saveroot = datapath / 'plots'
+        saveroot.mkdir(exist_ok=True)
+        fpath = saveroot / fname
         plt.savefig(str(fpath))
 
 
@@ -79,19 +82,44 @@ def blotches_all(id_, datapath=None):
                         wspace=0.001, hspace=0.001)
 
 
-def plot_clustered_fans(id_, scope='planet4', _dir=None, ax=None, **kwargs):
-    pm = io.PathManager(id_=id_, datapath=_dir)
-    if not pm.reduced_fanfile.exists():
-        print("Clustered/reduced fanfile not found")
-        return
-    reduced_fans = markings.FanContainer.from_fname(pm.reduced_fanfile,
-                                                    scope)
-    imgid = markings.ImageID(id_)
+def plot_clustered_fans(image_id, datapath=None, ax=None, scope_id=None,
+                        **kwargs):
+    """Plot the reduced/clustered fans.
 
-    imgid.plot_fans(fans=reduced_fans.content, ax=ax, **kwargs)
+    Plot the clustered fans for a planet4 image_id.
+    This will plot the extra stored only clustered fans before they are being
+    fnotched together.
+
+    Parameters
+    ----------
+    id_ : str
+        PlanetFour image id
+    datapath : pathlib.Path or str, optional
+        Path to folder where the analysis run is stored. If None, defaults to
+        data_root / 'clustering'
+    ax : matplotlib.axis, optional
+        Axis to plot on
+    scope_id : str
+        obsid (= image_name) to search in for image_id data.
+    """
+    if scope_id is not None:
+        pm = io.PathManager(id_=scope_id, datapath=datapath)
+        fans = pm.reduced_fandf
+        data = fans[fans.image_id == image_id]
+    else:
+        pm = io.PathManager(id_=image_id, datapath=datapath)
+        if not pm.reduced_fanfile.exists():
+            print("Clustered/reduced fanfile not found")
+            return
+        data = pm.reduced_fandf
+        # reduced_fans = markings.FanContainer.from_fname(pm.reduced_fanfile,
+        #                                                 scope='planet4')
+    imgid = markings.ImageID(image_id, scope='planet4')
+
+    imgid.plot_fans(fans=data, ax=ax, **kwargs)
 
 
-def fans_all(id_, _dir=None):
+def fans_all(id_, datapath=None):
     fig, axes = plt.subplots(ncols=2)
     plot_raw_fans(id_, ax=axes[0])
     plot_clustered_fans(id_, datapath, ax=axes[1])
