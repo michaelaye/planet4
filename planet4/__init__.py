e887fd01259779b27a69d5fd7e085ef2b45b491a
K.-Michael Aye <kmichael.aye@gmail.com>
2017-01-07 21:15:16 -0700

    reordered imports

--- planet4/__init__.py
+++ planet4/__init__.py
@@ -1,11 +1,10 @@
-import importlib
-import logging
-from pathlib import Path
-
 __version__ = '0.7.3'
 
-importlib.reload(logging)
-logpath = Path.home() / 'p4reduction.log'
-logging.basicConfig(filename=str(logpath), filemode='w',
-                    format='%(asctime)s %(levelname)s: %(message)s',
-                    datefmt='%Y-%m-%d %H:%M:%S')
+# import importlib
+# import logging
+# from pathlib import Path
+# importlib.reload(logging)
+# logpath = Path.home() / 'p4reduction.log'
+# logging.basicConfig(filename=str(logpath), filemode='w',
+#                     format='%(asctime)s %(levelname)s: %(message)s',
+#                     datefmt='%Y-%m-%d %H:%M:%S')
