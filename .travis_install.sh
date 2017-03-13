1cf0c626ac87b03f424220b4de3590cfe0f81bee
K.-Michael Aye <kmichael.aye@gmail.com>
2017-03-12 22:27:13 -0600

    put install commands into bash sript to be called by yaml

---
+++ .travis_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# this script is called by the install step in .travis.yml
+
+set -e
+
+echo 'List files from cached directories'
+echo 'pip:'
+ls $HOME/.cache/pip
+
+# deactivating the travis provided virtual env
+deactivate
+
+pushd .
+cd
+mkdir -p download
+cd download
+echo "Cached in $HOME/download :"
+ls -l
+echo
+if [[ ! -f miniconda.sh ]]
+  then
+    wget http://repo.continuum.io/miniconda/Miniconda-3.6.0-Linux-x86_64.sh \
+            -O miniconda.sh
+  fi
+chmod +x miniconda.sh && ./miniconda.sh -b
+cd ..
+export PATH=/home/travis/miniconda/bin:$PATH
+conda config --set always_yes yes --set changeps1 no
+conda update conda
+conda info -a
+popd
+conda create -q -n test-environment python=3
+source activate test-environment
+conda install --file requirements.txt
+conda install pytest
+pip install coveralls
+python setup.py install
+python -c "from planet4 import io;io.set_database_path('~')"
