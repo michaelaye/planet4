32f6d78049a084f78883e8bef3f7cf346c375522
K.-Michael Aye <kmichael.aye@gmail.com>
2017-03-16 22:47:35 -0600

    scope is default planet4

--- planet4/markings.py
+++ planet4/markings.py
@@ -92,7 +92,7 @@ class ImageID(object):
         If the data was already extracted before init, it can be provided here.
     """
 
-    def __init__(self, imgid, scope, dbname=None, data=None):
+    def __init__(self, imgid, scope='planet4', dbname=None, data=None):
         self.imgid = io.check_and_pad_id(imgid)
         if data is not None:
             self.data = data
