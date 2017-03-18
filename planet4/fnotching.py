5852a08f26efef20b7a89224aff907f7ea49af65
K.-Michael Aye <kmichael.aye@gmail.com>
2017-03-17 18:37:44 -0600

    implement L1B (fnotching) production

--- planet4/fnotching.py
+++ planet4/fnotching.py
@@ -1,92 +1,163 @@
 import logging
 
 import pandas as pd
-from numpy.linalg import norm
+import numpy as np
+from scipy.spatial.distance import cdist
 
-from . import markings
+from . import io, markings
 
 logger = logging.getLogger(__name__)
 
 
-def calc_fnotch(nfans, nblotches):
-    """Calculate the fnotch value (or fan-ness)."""
-    return (nfans) / (nfans + nblotches)
+def get_id_from_path(path):
+    return path.parent.name
 
 
-def do_the_fnotch(self):
-    """Combine fans and blotches if necessary.
-
-    Use `fnotch_distance` as criterion for linear algebraic distance between
-    average cluster markings to determine if they belong to a Fnotch, a
-    chimera object of indecision between a Fan and a Blotch, to be decided
-    later in the process by applying a `cut` on the resulting Fnotch
-    objects.
-
-    See Also
-    --------
-    markings.Fnotch : The Fnotch object with a `get_marking` method for a
-        `cut` value.
-    """
-    # check first if both blotchens and fans were found, if not, we don't
-    # need to fnotch.
-    if not all(self.reduced_data.values()):
-        logger.debug("CM: no fnotching required.")
-        self.fnotches = []
-        self.fnotched_blotches = self.reduced_data['blotch']
-        self.fnotched_fans = self.reduced_data['fan']
-        return
-
-    logger.debug("CM: do_the_fnotch")
-    n_close = 0
-    fnotches = []
-    blotches = []
-    fans = []
-    for blotch in self.reduced_data['blotch']:
-        for fan in self.reduced_data['fan']:
-            delta = blotch.center - fan.midpoint
-            if norm(delta) < self.fnotch_distance:
-                fnotch_value = calc_fnotch(fan.n_members, blotch.n_members)
-                fnotch = markings.Fnotch(fnotch_value, fan, blotch,
-                                         scope='hirise')
-                fnotch.n_fan_members = fan.n_members
-                fnotch.n_blotch_members = blotch.n_members
-                fnotches.append(fnotch)
-                n_close += 1
-                blotch.saved = True
-                fan.saved = True
-        # only after going through all fans for this one blotch, I can store it as an
-        # unfnotched blotch:
-        if not blotch.saved:
-            blotches.append(blotch)
-    # I have to wait until the loop over blotches is over, before I know that a fan really
-    # never was matched with a blotch, before I store it as an unfnotched Fan.
-    for fan in self.reduced_data['fan']:
-        if not fan.saved:
-            fans.append(fan)
-            fan.saved = True
-
-    self.fnotches = fnotches
-    self.fnotched_blotches = blotches
-    self.fnotched_fans = fans
-    logger.debug("CM: do_the_fnotch: Found %i fnotches.", n_close)
+def get_clusters_in_path(path):
+    "find csv files in path and combine into DataFrame"
+    clusters = []
+    id_ = get_id_from_path(path)
+    for kind in ['fans', 'blotches']:
+        df = pd.read_csv(str(path / f"L1A_{id_}_{kind}.csv"))
+        clusters.append(df)
+    return clusters
 
 
-def store_fnotched(self):
-    """Write out the clustered and fnotched data."""
-    logger.debug('CM: Writing output files.')
-    logger.debug('CM: Output dir: %s', self.datapath)
+def data_to_centers(df):
+    if all(np.isnan(df.distance)):
+        # only the blotch arrays have distance un-defined
+        Marking = markings.Blotch
+    elif all(np.isnan(df.radius_1)) and all(np.isnan(df.radius_2)):
+        Marking = markings.Fan
+    return np.vstack(Marking(row, scope='hirise').center for _, row in df.iterrows())
 
-    # first write the fnotched data
-    for outfname, outdata in zip(['fnotchfile', 'blotchfile', 'fanfile'],
-                                 [self.fnotches, self.fnotched_blotches,
-                                  self.fnotched_fans]):
-        if len(outdata) == 0:
+
+def fnotch_image_ids(obsid, eps=10):
+    "Cluster each image_id for an obsid separately."
+    # the clustering results were stored as L1A products
+    pm = io.PathManager(obsid=obsid)
+    paths = pm.get_obsid_paths('L1A')
+    for path in paths:
+        id_ = get_id_from_path(path)
+        print("Fnotching", id_)
+        fans, blotches = get_clusters_in_path(path)
+        distances = cdist(data_to_centers(fans), data_to_centers(blotches))
+        X, Y = np.where(distances < eps)
+        # X are the indices along the fans input, Y for blotches respectively
+
+        # loop over fans and blotches that are within `eps` pixels:
+        fnotches = []
+        for fan_loc, blotch_loc in zip(X, Y):
+            fan = fans.iloc[[fan_loc]]
+            blotch = blotches.iloc[[blotch_loc]]
+            fnotches.append(markings.Fnotch(fan, blotch).data)
+
+        # make sure the L1B folder exists
+        pm.id_ = id_
+        pm.reduced_fanfile.parent.mkdir(parents=True, exist_ok=True)
+
+        # store the combined fnotches into one file. The `votes_ratio` is
+        # stored as well, making it simple to filter/cut on these later for the
+        # L1C product.
+        try:
+            pd.concat(fnotches).to_csv(pm.fnotchfile)
+        except ValueError as e:
+            if e.args[0].startswith("No objects to concatenate"):
+                logger.debug("No fnotches found for %s.", id_)
+            else:
+                raise ValueError
+
+        # write out the fans and blotches that where not within fnotching distance:
+        fans.loc[set(fans.index) - set(X)].to_csv(pm.reduced_fanfile)
+        blotches.loc[set(blotches.index) - set(Y)].to_csv(pm.reduced_blotchfile)
+
+
+def apply_cut(obsid, cut=0.5):
+    pm = io.PathManager(obsid=obsid)
+    paths = pm.get_obsid_paths('L1B')
+    for path in paths:
+        id_ = get_id_from_path(path)
+        pm.id_ = id_
+        try:
+            fnotches = pd.read_csv(pm.fnotchfile, index_col=0)
+        except FileNotFoundError:
             continue
-        # get the path from PathManager object
-        series = [cluster.store() for cluster in outdata]
-        df = pd.DataFrame(series)
-        self.save(df, getattr(self.pm, outfname))
+        return fnotches
 
+# def do_the_fnotch(self):
+#     """Combine fans and blotches if necessary.
+#
+#     Use `fnotch_distance` as criterion for linear algebraic distance between
+#     average cluster markings to determine if they belong to a Fnotch, a
+#     chimera object of indecision between a Fan and a Blotch, to be decided
+#     later in the process by applying a `cut` on the resulting Fnotch
+#     objects.
+#
+#     See Also
+#     --------
+#     markings.Fnotch : The Fnotch object with a `get_marking` method for a
+#         `cut` value.
+#     """
+#     # check first if both blotchens and fans were found, if not, we don't
+#     # need to fnotch.
+#     if not all(self.reduced_data.values()):
+#         logger.debug("CM: no fnotching required.")
+#         self.fnotches = []
+#         self.fnotched_blotches = self.reduced_data['blotch']
+#         self.fnotched_fans = self.reduced_data['fan']
+#         return
+#
+#     logger.debug("CM: do_the_fnotch")
+#     n_close = 0
+#     fnotches = []
+#     blotches = []
+#     fans = []
+#     for blotch in self.reduced_data['blotch']:
+#         for fan in self.reduced_data['fan']:
+#             delta = blotch.center - fan.midpoint
+#             if norm(delta) < self.fnotch_distance:
+#                 fnotch_value = calc_fnotch(fan.n_members, blotch.n_members)
+#                 fnotch = markings.Fnotch(fnotch_value, fan, blotch,
+#                                          scope='hirise')
+#                 fnotch.n_fan_members = fan.n_members
+#                 fnotch.n_blotch_members = blotch.n_members
+#                 fnotches.append(fnotch)
+#                 n_close += 1
+#                 blotch.saved = True
+#                 fan.saved = True
+#         # only after going through all fans for this one blotch, I can store it as an
+#         # unfnotched blotch:
+#         if not blotch.saved:
+#             blotches.append(blotch)
+#     # I have to wait until the loop over blotches is over, before I know that a fan really
+#     # never was matched with a blotch, before I store it as an unfnotched Fan.
+#     for fan in self.reduced_data['fan']:
+#         if not fan.saved:
+#             fans.append(fan)
+#             fan.saved = True
+#
+#     self.fnotches = fnotches
+#     self.fnotched_blotches = blotches
+#     self.fnotched_fans = fans
+#     logger.debug("CM: do_the_fnotch: Found %i fnotches.", n_close)
+#
+
+# def store_fnotched(self):
+#     """Write out the clustered and fnotched data."""
+#     logger.debug('CM: Writing output files.')
+#     logger.debug('CM: Output dir: %s', self.datapath)
+#
+#     # first write the fnotched data
+#     for outfname, outdata in zip(['fnotchfile', 'blotchfile', 'fanfile'],
+#                                  [self.fnotches, self.fnotched_blotches,
+#                                   self.fnotched_fans]):
+#         if len(outdata) == 0:
+#             continue
+#         # get the path from PathManager object
+#         series = [cluster.store() for cluster in outdata]
+#         df = pd.DataFrame(series)
+#         self.save(df, getattr(self.pm, outfname))
+#
 
 # def execute_pipeline(self, data):
 #     """Execute the standard list of methods for catalog production.
