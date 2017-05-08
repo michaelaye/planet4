38969c6d770deacba70542b29b8f283ec5d3bcd4
K.-Michael Aye <kmichael.aye@gmail.com>
2017-05-07 22:04:38 -0600

    first commit

---
+++ planet4/science/size_distribution.py
@@ -0,0 +1,7 @@
+from planet4 import io, region_data, markings
+from little_helpers.p4_tools import get_final_markings_counts
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.stats as stats
+from scipy.optimize import root, curve_fit, leastsq
