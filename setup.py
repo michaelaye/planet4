734f994bcb847d99011a381c83c0ea6780961109
K.-Michael Aye <kmichael.aye@gmail.com>
2018-11-21 18:28:06 -0700

    Bump version: 0.11.0 -> 0.11.1

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name=DISTNAME,
-    version='0.11.0',
+    version='0.11.1',
     packages=find_packages(),
 
     install_requires=['pandas', 'numpy', 'matplotlib', 'pyaml',
